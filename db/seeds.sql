595f1c36822844c6d7c1dc6ec4399ea1576397bc
dieres <dieres2010@gmail.com>
2022-03-22 20:07:56 -0400

    Users Enabled to Vote

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, email, password) VALUES
+		("JhonDoe", "Jdoe@gmail.com", "jdoe1234"),
+        ("Jetli", "Jli@gmail.com", "Jli1234"),
+	    ("Wutang", "Wtang@gmail.com", "Wtang1234");
+
+INSERT INTO posts (title, post_url, user_id) VALUES
+    ("Taskmaster goes public!","https://taskmaster.com/press", 1),
+    ("Runbuddy reaches 1 million subscribers","https://runbuddy.com/press", 1);
